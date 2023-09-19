17b7707965a8301169770326103c164a8f0b5638
Ubaton <raymondNG729@gmail.com>
2023-09-19 14:04:22 +0200

    v0.1.10

--- src/components/pages/Home/Homepage.js
+++ src/components/pages/Home/Homepage.js
@@ -25,7 +25,9 @@ const Homepage = () => {
               <img src={Logo} alt="Logo" className="max-w-full " />
             </div>
           </div>
+        </div>
 
+        <div className=" pt-20 ">
           <div className="flex flex-col space-y-4 md:flex-row md:space-x-4">
             <Link to="/services">
               <button className="w-full md:w-auto text-zinc-950 bg-customColor-action hover:bg-customColor-hover py-2 px-4 rounded-lg focus:outline-none focus:ring focus:border--700 transition duration-300">
@@ -33,16 +35,21 @@ const Homepage = () => {
               </button>
             </Link>
           </div>
-        </div>
 
-        {/* Latest Offers Section */}
-        <section className=" bg-customColor-cardcolor text-customColor-colortext p-4 rounded-lg shadow-lg mt-6">
-          <h2 className="text-2xl font-semibold mb-2">Latest Offers</h2>
-          <p>Check out our special offers and promotions for a limited time.</p>
-          <Link to="/offerselection" className="text-amber-500 hover:underline">
-            View Offers
-          </Link>
-        </section>
+          {/* Latest Offers Section */}
+          <section className=" bg-customColor-cardcolor text-customColor-colortext p-4 rounded-lg shadow-lg mt-6">
+            <h2 className="text-2xl font-semibold mb-2">Latest Offers</h2>
+            <p>
+              Check out our special offers and promotions for a limited time.
+            </p>
+            <Link
+              to="/offerselection"
+              className="text-amber-500 hover:underline"
+            >
+              View Offers
+            </Link>
+          </section>
+        </div>
       </div>
     </div>
   );
